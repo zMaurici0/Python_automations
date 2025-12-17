a60233b9a6e7e3f07172cd85c1ae67d181d5aa1f
zMaurici0 <zmauriciomota@gmail.com>
2025-12-17 09:12:47 -0400

    Automação de envio de emails com Python

---
+++ Envio-de-Emails/sendEmail.py
@@ -0,0 +1,36 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+import os
+from dotenv import load_dotenv
+
+
+# carrega variáveis de ambiente
+
+load_dotenv()
+
+password = os.getenv('EMAIL_PASSWORD')
+
+fromEmail = 'automacaotestepython@gmail.com'
+toEmail = 'zmauriciomota@gmail.com'
+subject = 'Automação com Python'
+body = open('Envio-de-Emails/body.txt', 'r', encoding='utf-8').read()
+
+# 1 - Definindo mensagem
+
+mensagem = EmailMessage()
+mensagem['From'] = fromEmail
+mensagem['To'] = toEmail
+mensagem['Subject'] = subject
+mensagem.set_content(body)
+safe = ssl.create_default_context()
+
+# 2 - enviando o email
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=safe) as smtp:
+    smtp.login(fromEmail, password)
+    smtp.sendmail(
+        fromEmail,
+        toEmail,
+        mensagem.as_string()
+    )
